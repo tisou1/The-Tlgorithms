79546407f606de45dd6c725b06a363f2c04033ea
tisou1 <57515396+tisou1@users.noreply.github.com>
2022-07-07 22:44:20 +0800

    chore(js): Stack es6

---
+++ packages/javascript/Data-structures/Stack/index.ts
@@ -0,0 +1,41 @@
+
+
+
+
+class Stack<T> {
+  stack: T[]
+  top: number
+  constructor() {
+    this.stack = []
+    this.top = 0
+  }
+
+  push(newValue: T) {
+    this.stack.push(newValue)
+    this.top += 1
+  }
+
+  pop() {
+    if(this.top !== 0) {
+      this.top -= 1
+      return this.stack.pop()
+    }
+    throw new Error('Stack Underflow')
+  }
+
+  get length():number {
+    return this.top
+  }
+
+  get isEmpty():boolean {
+    return this.top === 0
+  }
+
+  get last() {
+    if(this.top !== 0)
+    return this.stack[this.stack.length - 1]
+  return null
+  }
+
+  
+}
