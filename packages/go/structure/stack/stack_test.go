45a41a54917057bbcf8b30954de47066359d57a3
tisou1 <57515396+tisou1@users.noreply.github.com>
2022-07-10 16:28:45 +0800

    chore(go): stack test

--- packages/go/structure/stack/stack_test.go
+++ packages/go/structure/stack/stack_test.go
@@ -1,6 +1,7 @@
 package stack
 
 import (
+	"container/list"
 	"fmt"
 	"reflect"
 	"testing"
@@ -10,6 +11,7 @@ func Hello() {
 	fmt.Println("Hello, World!")
 }
 
+//array类型stack测试
 func TestStackArray(t *testing.T) {
 	t.Run("Stack With Array", func(t *testing.T) {
 
@@ -59,5 +61,103 @@ func TestStackArray(t *testing.T) {
 			}
 		})
 
+	})
+}
+
+//节点stack测试函数
+func TestStackNode(t *testing.T) {
+	var newStack Stack
+	newStack.push(1)
+	newStack.push(2)
+
+	t.Run("Stack Push", func(t *testing.T) {
+		result := newStack.show()
+		expect := []any{2, 1}
+		for x := range result {
+			if result[x] != expect[x] {
+				t.Errorf("栈的push操作出错,期望 %v 实际 %v", expect, result)
+			}
+		}
+	})
+
+	t.Run("Stack empty", func(t *testing.T) {
+		if newStack.len() != 2 {
+			t.Errorf("栈的empty操作出错,期望 %v 实际 %v", 2, newStack.len())
+		}
+	})
+
+	newStack.pop()
+	pop := newStack.pop()
+	t.Run("Stack Pop", func(t *testing.T) {
+		if pop != 1 {
+			t.Errorf("栈的pop操作出错,期望 %v 实际 %v", 1, pop)
+		}
+	})
+
+	newStack.push(3)
+	newStack.push(4)
+	newStack.push(5)
+	t.Run("Stack Peak", func(t *testing.T) {
+		if newStack.peak() != 2 {
+			t.Error("栈的peak操作出错,顶部为5,实际得到", newStack.peak())
+		}
+	})
+}
+
+//list库的stack测试函数
+func TestStackLinkedListWithList(t *testing.T) {
+	stackList := &SList{
+		stack: list.New(),
+	}
+
+	t.Run("Stack Push", func(t *testing.T) {
+		stackList.Push(2)
+		stackList.Push(3)
+
+		if stackList.Length() != 2 {
+			t.Errorf("栈的push操作出错,期望 %v 实际 %v", 2, stackList.Length())
+		}
+	})
+
+	t.Run("Stack Pop", func(t *testing.T) {
+		pop, _ := stackList.Pop()
+
+		if stackList.Length() != 1 && pop != 3 {
+			t.Errorf("栈的pop操作出错,期望 %v 实际 %v", 3, pop)
+		}
+	})
+
+	t.Run("Stack Peak", func(t *testing.T) {
+		stackList.Push(2)
+		stackList.Push(83)
+
+		peak, _ := stackList.Peak()
+		if peak != 83 {
+			t.Errorf("栈的peak操作出错,期望 %v 实际 %v", 83, peak)
+		}
+	})
+
+	t.Run("Stack Length", func(t *testing.T) {
+		if stackList.Length() != 3 {
+			t.Errorf("栈的length操作出错,期望 %v 实际 %v", 3, stackList.Length())
+		}
+	})
+
+	t.Run("Stack Empty", func(t *testing.T) {
+		if stackList.Empty() {
+			t.Errorf("栈的empty操作出错,期望 %v 实际 %v", false, stackList.Empty())
+		}
+
+		d1, err := stackList.Pop()
+		d2, _ := stackList.Pop()
+		d3, _ := stackList.Pop()
+
+		if err != nil {
+			t.Errorf("got an unexpected error %v, pop1: %v, pop2: %v, pop3: %v", err, d1, d2, d3)
+		}
+
+		if !stackList.Empty() {
+			t.Errorf("栈的empty操作出错,期望 %v 实际 %v", true, stackList.Empty())
+		}
 	})
 }
