fe4d9c29abc72e84cd7a54b0e765db9b866c6856
tisou1 <57515396+tisou1@users.noreply.github.com>
2022-07-25 22:26:43 +0800

    feature(go): 插入排序

---
+++ packages/go/structure/sort/insertionsort.go
@@ -0,0 +1,16 @@
+package sort
+
+import "gp-algorithms/constraints"
+
+//插入排序
+func Insertion[T constraints.Ordered](arr []T) []T {
+	for currentIndex := 1; currentIndex < len(arr); currentIndex++ {
+		temporary := arr[currentIndex]
+		iterator := currentIndex
+		for ; iterator > 0 && arr[iterator-1] > temporary; iterator-- {
+			arr[iterator] = arr[iterator-1]
+		}
+		arr[iterator] = temporary
+	}
+	return arr
+}
