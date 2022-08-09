74ccd0882742877903dd9628de350984cc2baa42
tisou1 <57515396+tisou1@users.noreply.github.com>
2022-08-09 17:54:11 +0800

    feat: maxHeap

--- packages/javascript/Data-structures/heap/maxHeap.ts
+++ packages/javascript/Data-structures/heap/maxHeap.ts
@@ -4,23 +4,75 @@
 
 export class BinaryHeap<T>{
 
-  heap: T[]
+  heap: T[][]
 
   constructor() {
     this.heap = []
-  }
-
-  inser(value: T) {
-    this.heap.push(value)
-    //调整
-    this.heapify()
   }
 
   get size() {
     return this.heap.length
   }
 
+  empty = () => this.heap.length
 
-  //TODO
-  heapify(){}
+  print = () => this.heap
+
+  insert(value: T[]) {
+    this.heap.push(value)
+    //调整
+    this.heapify()
+    return this.size
+  }
+
+
+
+
+  // 重新排序
+  heapify(){
+    let index = this.size - 1
+
+    while(index > 0) {
+      const element = this.heap[index]
+      //最后一个非叶子节点
+      const parentIndex = Math.floor((index - 1) / 2)
+      const parent = this.heap[parentIndex]
+
+      if(parent[0] >= element[0]) break
+      this.heap[index] = parent
+      this.heap[parentIndex] = element
+      index = parentIndex
+    }
+  }
+
+  //取出最大的
+  extractMax() {
+    const max = this.heap[0]
+    const tmp = this.heap.pop()
+    if(!this.empty()) {
+      this.heap[0] = tmp as T[]
+      this.sinkDown(0)
+    }
+    return max
+  }
+
+  //在提取后 恢复平衡
+  sinkDown(index) {
+    const left = 2 * index + 1
+    const right = 2 * index + 2
+    let largest = index
+    const length = this.size
+
+    if(left < length && this.heap[left][0] > this.heap[largest][0]) {
+      largest = left
+    }
+    if(right < length && this.heap[right][0] > this.heap[largest][0]) {
+      largest = right
+    }
+
+    if(largest !== index) {
+      [this.heap[largest], this.heap[index]] = [this.heap[index], this.heap[largest]]
+      this.sinkDown(largest)
+    }
+  }
 }
--- packages/javascript/Data-structures/heap/test/maxHeap.test.ts
+++ packages/javascript/Data-structures/heap/test/maxHeap.test.ts
@@ -7,9 +7,23 @@ describe('Maxheap', () => {
   beforeEach(() => {
     heap = new BinaryHeap()
   })
-1n
+
   it('size', () => {
     expect(heap.size).toMatchInlineSnapshot('0')
+    expect(heap.empty()).toBe(0)
+    
+    expect(heap.insert([1])).toBe(1)
+    heap.insert([4])
+    heap.insert([3])
+    heap.insert([6])
+    heap.insert([1])
+    heap.insert([8])
+    heap.insert([2])
+    expect(heap.extractMax()).toMatchInlineSnapshot(`
+      [
+        8,
+      ]
+    `)
   })
 
 })
